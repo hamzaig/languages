240c7a4c223c2dfe60e20c7f8f6d89406a95ec7b
Hamza Ali Khalid <hamzaig@yahoo.com>
2024-09-25 19:17:17 +0500

    x

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const languages = require("./languages");
 const defaultl = require("./defaultLangLocales");
 
 exports.isValid = function (langcode) {
-  const language = languages.find(lang => lang.languageCodes === langcode.toLowerCase());
+  const language = languages.find(
+    (lang) => lang.languageCodes === langcode.toLowerCase()
+  );
 
   if (language) {
     return `${language.name} (${langcode}) is a valid language code.`;
@@ -11,33 +13,35 @@ exports.isValid = function (langcode) {
 };
 
 exports.getLanguageName = function (langcode) {
-  const language = languages.find(lang => lang.languageCodes === langcode.toLowerCase());
-  return language ? language.name : null; 
+  const language = languages.find(
+    (lang) => lang.languageCodes === langcode.toLowerCase()
+  );
+  return language ? language.name : null;
 };
 
 exports.checkTranslationAvailability = function (code) {
-  const matchedLanguages = languages.filter(lang => lang.languageCodes === code);
+  const matchedLanguages = languages.filter(
+    (lang) => lang.languageCodes === code
+  );
 
   if (matchedLanguages.length > 0) {
-    
-    return matchedLanguages.map(language => ({
+    return matchedLanguages.map((language) => ({
       name: language.name,
-      locale: language.locale,  
+      locale: language.locale,
       googleTranslate: !!language.googleTranslate,
       microsoftTranslate: !!language.microsoftTranslate,
       deeplTranslate: !!language.deeplTranslate,
       awsTranslate: !!language.awsTranslate,
       ibmTranslate: !!language.ibmTranslate,
-      awsTranscribe: !!language.awsTranscribe ? language.awsTranscribe.code : false,
-      googleStt: !!language.googleStt ? language.googleStt.code  : false,
-      googleTTS: !!language.googleTTS ? language.googleTTS.code  : false,
-      awsPolly: !!language.awsPolly ? language.awsPolly.code  : false
+      awsTranscribe: !!language.awsTranscribe
+        ? language.awsTranscribe.code
+        : false,
+      googleStt: !!language.googleStt ? language.googleStt.code : false,
+      googleTTS: !!language.googleTTS ? language.googleTTS.code : false,
+      awsPolly: !!language.awsPolly ? language.awsPolly.code : false,
     }));
   }
   return null;
 };
 
-
-console.log(exports.checkTranslationAvailability('en'));
-
-
+module.exports = helloNpm;
